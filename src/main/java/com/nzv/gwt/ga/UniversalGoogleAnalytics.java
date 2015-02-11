8ffef9fe86e70d26ba7b28fcac67641b66635591
nicolas.vallet <nicolas.vallet@gmail.com>
2015-02-11 10:15:07 +0100

    Repackage the whole project.

---
+++ src/main/java/com/nzv/gwt/ga/UniversalGoogleAnalytics.java
@@ -0,0 +1,17 @@
+package com.nzv.gwt.ga;
+
+public interface UniversalGoogleAnalytics {
+	
+	public void init(String userAccount);
+	
+	public void trackPageview();
+	
+	public void trackPageview(String pageName);
+	
+	public void trackEvent(String category, String action);
+	
+	public void trackEvent(String category, String action, String label);
+	
+	public void trackEvent(String category, String action, String label, int value);
+
+}
